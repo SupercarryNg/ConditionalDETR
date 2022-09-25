4c51db9a0b856a60175707eb3f7163bbfca45bcc
Junyan <u3591537@connect.hku.hk>
2022-09-25 11:00:25 +0800

    supervise the unk obj loss topk finish

--- models/conditional_detr.py
+++ models/conditional_detr.py
@@ -143,18 +143,17 @@ class SetCriterion(nn.Module):
 
         idx = self._get_src_permutation_idx(indices)
         target_classes_o = torch.cat([t["labels"][J] for t, (_, J) in zip(targets, indices)])
-        target_classes = torch.full(src_logits.shape[:2], self.num_classes,
+        target_classes = torch.full(src_logits.shape[:2], self.num_classes - 1,
                                     dtype=torch.int64, device=src_logits.device)
         target_classes[idx] = target_classes_o
 
-        target_classes_onehot = torch.zeros([src_logits.shape[0], src_logits.shape[1], src_logits.shape[2] + 1],
+        target_classes_onehot = torch.zeros([src_logits.shape[0], src_logits.shape[1], src_logits.shape[2]],
                                             dtype=src_logits.dtype, layout=src_logits.layout, device=src_logits.device)
         target_classes_onehot.scatter_(2, target_classes.unsqueeze(-1), 1)
 
-        target_classes_onehot = target_classes_onehot[:, :, :-1]
-
-        # target_classes_onehot[self.unk_topk_indices, -1] = 1
-
+        # supervise unknown obj idx
+        for i, unk_topk_idx in enumerate(self.unk_topk_indices):
+            target_classes_onehot[i, unk_topk_idx, -1] = 0
 
         loss_ce = sigmoid_focal_loss(src_logits, target_classes_onehot, num_boxes, alpha=self.focal_alpha, gamma=2) * \
                   src_logits.shape[1]
